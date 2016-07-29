b4c6350c18b917e7b7a162c1e553817221cd7873
huangchao <ftchao@local.com.cn>
2016-07-29 19:34:15 +0800

    del echo

--- meego.go
+++ meego.go
@@ -1,14 +1,11 @@
 package meego
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func Run(addr string) {
-	fmt.Print("meego.run\n")
-
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
